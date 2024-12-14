5c56c1b81aa37452987a24f203ae8fa0da4c5f12
Refki Alfarizi <refkialfarizi123@gmail.com>
2024-12-14 17:57:59 +0700

    feat: integrated folder

--- integrated_main.py
+++ integrated_main.py
@@ -2,6 +2,10 @@ import sys
 from src.ui.home_ui import HomeUI
 from src.ui.calendar_ui import CalendarUi
 from src.ui.progress_ui import ProgressWindow
+from src.ui.folder_ui import FileFolderUI
+from src.ui.add_file_ui import AddFileUI
+from src.controllers.folder_controller import FolderController
+from src.controllers.add_file_controller import AddFileController
 from src.controllers.home_controller import HomeController
 from src.components.navbar import NavBar
 from src.controllers.calendar_controller import CalendarController
@@ -38,7 +42,8 @@ class MainWindow(QMainWindow):
         # Add pages to stacked widget
         self.home_ui = HomeUI()
         self.home_controller = HomeController(self.home_ui)
-        self.folder_ui = QWidget()  # Replace with your actual folder UI
+        self.folder_ui = FileFolderUI()
+        self.add_file_ui = AddFileUI()
         self.calendar_ui = CalendarUi("tasks.db")  # Replace with your actual calendar UI
         self.progress_ui = ProgressWindow()  # Replace with your actual progress UI
         self.progress_ui.set_progress_controller(ProgressController(self.progress_ui, "tasks.db"))
@@ -47,6 +52,11 @@ class MainWindow(QMainWindow):
         self.stacked_widget.addWidget(self.folder_ui)
         self.stacked_widget.addWidget(self.calendar_ui)
         self.stacked_widget.addWidget(self.progress_ui)
+        self.stacked_widget.addWidget(self.add_file_ui)
+
+        # Create controllers
+        self.folder_controller = FolderController(self.folder_ui, self.add_file_ui)
+        self.add_file_controller = AddFileController(self.add_file_ui)
         
         # Set window properties
         self.setMinimumSize(1280, 720)
@@ -56,11 +66,20 @@ class MainWindow(QMainWindow):
         self.navbar.folder_clicked.connect(lambda: self.switch_page(self.folder_ui))
         self.navbar.calendar_clicked.connect(lambda: self.switch_page(self.calendar_ui))
         self.navbar.progress_clicked.connect(lambda: self.switch_page(self.progress_ui))
+        self.folder_ui.add_button.clicked.connect(self.show_add_file)
+        self.add_file_ui.pushButtonBatal.clicked.connect(self.show_folder)
         
     def switch_page(self, page):
         """Switch to the given page."""
         self.stacked_widget.setCurrentWidget(page)
-        
+
+    def show_folder(self):
+        self.folder_controller.load_files_from_database()
+        self.stacked_widget.setCurrentWidget(self.folder_ui)
+
+    def show_add_file(self):
+        # Show the add file view
+        self.stacked_widget.setCurrentWidget(self.add_file_ui)       
 
 if __name__ == '__main__':
     
